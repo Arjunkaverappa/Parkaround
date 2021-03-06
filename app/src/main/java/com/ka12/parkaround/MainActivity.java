885beb21d8f3983e5126fe4ecc1e8da4f0e523d1
arjunkaverappa <www.arjunkaverappa@gmail.com>
2021-03-06 20:11:35 +0530

    Initial commit
    Signed-off-by: arjunkaverappa <www.arjunkaverappa@gmail.com>

---
+++ app/src/main/java/com/ka12/parkaround/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ka12.parkaround;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
+/*
+   Additional imfo about the project
+ */
---
+++ app/src/main/java/com/ka12/parkaround/Splash_screen.java
@@ -0,0 +1,25 @@
+package com.ka12.parkaround;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent in = new Intent(Splash_screen.this, MainActivity.class);
+            startActivity(in);
+            Animatoo.animateZoom(Splash_screen.this);
+            finish();
+        }, 2000);
+    }
+}
