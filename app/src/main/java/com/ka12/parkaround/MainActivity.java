46c43c02316c95b1f87c8a18f4774590296ef17f
arjunkaverappa <www.arjunkaverappa@gmail.com>
2021-03-06 20:11:35 +0530

    Initial commit

---
+++ app/src/main/java/com/ka12/parkaround/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ka12.parkaround;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
